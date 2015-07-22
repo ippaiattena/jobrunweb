a221db91592ee83b2981a7d65665831494cfc21d
ippaiattena <ippaiattena@outlook.com>
2015-07-22 23:08:26 +0900

    add log write
    log書き出し処理追加

--- jobrunweb/Default.aspx.cs
+++ jobrunweb/Default.aspx.cs
@@ -10,7 +10,9 @@ namespace jobrunweb
 {
     public partial class Default : Page
     {
-        dynamic json = null;
+        dynamic _json = null;
+        string _path = null;
+        Encoding _enc = Encoding.GetEncoding("Shift_JIS");
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -21,20 +23,21 @@ namespace jobrunweb
                 string jsonValue = reader.ReadToEnd();
                 reader.Close();
                 // DynamicJson
-                json = DynamicJson.Parse(jsonValue);
+                _json = DynamicJson.Parse(jsonValue);
+                _path = _json.batPath;
 
                 // 認証キー取得
                 string key = Page.Request.QueryString.Get("key");
 
                 // 確認 => 不一致なら終了
-                if (string.IsNullOrEmpty(key) == true) return;
-                if (key != json.getKey) return;
+                if (string.IsNullOrEmpty(key) == true) throw new Exception("unmatch:getkey");
+                if (key != _json.getKey) throw new Exception("unmatch:json_getkey");
 
                 // 実行ジョブ名取得
                 string bat = Page.Request.QueryString.Get("bat");
 
                 // ジョブ名がなければ終了
-                if (string.IsNullOrEmpty(bat) == true) return;
+                if (string.IsNullOrEmpty(bat) == true) throw new Exception("unmatch:getbat");
 
                 if (bat == "start" || bat == "stop")
                 {
@@ -44,16 +47,32 @@ namespace jobrunweb
                 else
                 {
                     // ProcessにコマンドプロンプトとBATアドレスを指定して実行
-                    string Argument = json.batPath + @"Bat\" + bat + ".bat";
+                    string Argument = _json.batPath + @"Bat\" + bat + ".bat";
                     string Filename = Environment.GetEnvironmentVariable("ComSpec");
                     runProcess("/c \"" + Argument + "\"", Filename);
                 }
                 // IFTTT MAKERへPOST
                 runPost(bat);
+
+                // Logへ書き込み
+                using (StreamWriter writer = new StreamWriter(_json.batPath + @"Log\log.txt", true, _enc))
+                {
+                    writer.WriteLine(DateTime.Now.ToString() + bat.ToString());
+                }
             }
             catch (Exception ex)
             {
-                Response.Write(ex.ToString());
+                if (string.IsNullOrEmpty(_path) == true)
+                {
+                    Response.Write(ex.ToString());
+                }
+                else
+                {
+                    using (StreamWriter writer = new StreamWriter(_json.batPath + @"Log\log.txt", true, _enc))
+                    {
+                        writer.WriteLine(DateTime.Now.ToString() + ex.ToString());
+                    }
+                }
             }
         }
 
@@ -82,8 +101,8 @@ namespace jobrunweb
         protected void runUdp(string command,string no)
         {
             //データを送信するリモートホストとポート番号
-            string remoteHost = json.remoteHost;
-            int remotePort = json.remotePort;
+            string remoteHost = _json.remoteHost;
+            int remotePort = _json.remotePort;
 
             //UdpClientオブジェクトを作成する
             System.Net.Sockets.UdpClient udp = new System.Net.Sockets.UdpClient();
@@ -109,19 +128,17 @@ namespace jobrunweb
         /// <param name="bat">BATコマンド名</param>
         protected void runPost(string bat)
         {
-            string makerKey = json.makerKey;
-            string makerTrigger = json.makerTrigger;
+            string makerKey = _json.makerKey;
+            string makerTrigger = _json.makerTrigger;
             // IFTTT MAKER
             string url = "http://maker.ifttt.com/trigger/" + makerTrigger + "/with/key/" + makerKey;
 
-            Encoding enc = Encoding.GetEncoding("shift_jis");
-
             //POST送信する文字列を作成
-            string postData = "value1=" + System.Web.HttpUtility.UrlEncode(bat, enc);
+            string postData = "value1=" + System.Web.HttpUtility.UrlEncode(bat, _enc);
 
             WebClient wc = new WebClient();
             //文字コードを指定する
-            wc.Encoding = enc;
+            wc.Encoding = _enc;
             //ヘッダにContent-Typeを加える
             wc.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
             //データを送信し、また受信する
